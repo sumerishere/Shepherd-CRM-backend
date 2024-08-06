ce912af5fc96c0f96e6740c0402f948f8b82fc88
Pathan Sumer Khan <ksumer597@gmail.com>
2024-08-07 01:43:23 +0530

    PUT Api in-process

--- src/main/java/com/template/controller/DataTableController.java
+++ src/main/java/com/template/controller/DataTableController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,10 @@ public class DataTableController {
 		 return dataTableService.getTemplateData(formtemplateId);
 	}
 	    
+	@PutMapping("/update-user-info")
+	public ResponseEntity<?> UpdateUserInfo(){
+		return null;
+	}
 	
 	@DeleteMapping("/delete-by-uid/{UID}")
 	public ResponseEntity<?> deleteUserByDataTableId(@PathVariable("UID") Long UID){
--- src/main/java/com/template/controller/FormTemplateController.java
+++ src/main/java/com/template/controller/FormTemplateController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,7 @@ public class FormTemplateController {
 		return formTemplateService.getTemplateByUsername(userName);
 	}
 	
+
 	
 		
 }
